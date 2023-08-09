e14a27a51f6f29b8050eed6ccae52b53f270dcb2
IlushinGL <ilushingl@yandex.ru>
2023-08-10 02:57:58 +0300

    feat: захардкодить _id

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const {
   UNAUTHORIZED,
 } = require('http-status-codes').StatusCodes;
-const { secretKey } = require('../utils/constants').SECRETS;
+const { secretKey } = require('../utils/constants');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
