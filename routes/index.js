245fff19c2fa90eed790b94d6647d4b1df0aa1c3
IlushinGL <ilushingl@yandex.ru>
2023-08-10 06:32:57 +0300

    feat: проверять авторизацию

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { celebrate, Joi, errors } = require('celebrate');
 const {
+  INTERNAL_SERVER_ERROR,
+} = require('http-status-codes').StatusCodes;
+const {
   newUser,
   login,
 } = require('../controllers/user');
@@ -19,5 +22,19 @@ router.post('/signin', login);
 router.use(auth);
 router.use('/users', require('./user'));
 router.use('/cards', require('./card'));
+
+router.use((err, req, res) => {
+  // если у ошибки нет статуса, выставляем INTERNAL_SERVER_ERROR
+  const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === INTERNAL_SERVER_ERROR
+        ? 'Сервер не смог обработать ошибку'
+        : message,
+    });
+});
 
 module.exports = router;
