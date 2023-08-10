29a50a947e9bda89ec36455801d9069379f960b6
IlushinGL <ilushingl@yandex.ru>
2023-08-10 07:46:31 +0300

    feat: проверять запрос

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.post('/signup', celebrate({
     email: Joi.string().email(),
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().domain(),
   }).unknown(true),
 }), newUser);
 
