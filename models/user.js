951c90c3515f995cc65cac6f907d5f0b8cfbe773
IlushinGL <ilushingl@yandex.ru>
2023-08-10 09:40:01 +0300

    feat: проверять запрос

--- models/user.js
+++ models/user.js
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.findUserByCredentials = function (email, password) {
   const errCredentials = new Error('Почта или пароль указаны не верно');
   // errCredentials.statusCode = 401;
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         // пользователь не найден — отклоняем промис с ошибкой
