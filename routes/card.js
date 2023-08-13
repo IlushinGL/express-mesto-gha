ce4feadbc66c539a98ba6be8deac884f6a94c0b6
IlushinGL <ilushingl@yandex.ru>
2023-08-13 18:07:40 +0300

    feat: обработать post /cards

--- routes/card.js
+++ routes/card.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getAllCards,
   deleteCard,
@@ -7,8 +9,15 @@ const {
   dislikeCard,
 } = require('../controllers/card');
 
+const validUrl = require('../utils/validators');
+
 router.get('/', getAllCards);
-router.post('/', newCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string().pattern(validUrl()),
+  }).unknown(true),
+}), newCard);
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
