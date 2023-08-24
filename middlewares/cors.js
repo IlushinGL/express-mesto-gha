875bfbf73cc31af80b1713e9f6e4135854bf9900
IlushinGL <ilushingl@yandex.ru>
2023-08-24 17:31:31 +0300

    feat: обрабатывать CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,20 +1,33 @@
-const { NODE_ENV = 'develoup' } = process.env;
+const { NODE_ENV = 'develoupment' } = process.env;
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// TODO: Подумать как вынести список в .env
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
+  'https://mesto.iknow.studio',
+  'http://mesto.iknow.studio',
+  'http://127.0.0.1:3000',
 ];
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { method } = req; // HTTP-метод
+  const { origin } = req.headers; // источник запроса
+  // список заголовков исходного запроса
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // указываем разрешенные типы кросс-доменных запросов
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // подтверждаем заголовки запроса
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // возвращаем ответ клиенту
+    return res.end();
+  }
   // проверяем режим
   if (NODE_ENV !== 'production') {
-    // устанавливаем заголовок, который разрешает браузеру запросы с любого источника
+    // разрешить запросы с любого источника
     res.header('Access-Control-Allow-Origin', '*');
   } else if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    // разрешить запросы с указанного источника
     res.header('Access-Control-Allow-Origin', origin);
   }
-
   next();
 };
